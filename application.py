e86c3477065c3569d74af92d70f805e300c0de37
harisonharold <harisonharold55@gmail.com>
2023-04-01 13:12:16 -0400

    Skeleton

---
+++ application.py
@@ -0,0 +1,28 @@
+import random
+
+class Program:
+    def __init__(self) -> None:
+        pass
+    def run():
+         pass
+    def showMainMenu():
+        def openAccount():
+            acc = random.randrange(4,4.9)
+            print("Your account number is:",acc)
+            pass
+        def selectAccount():
+            pass
+        def exitMenu():
+            pass
+    def showAccountMenu():
+        def checkBalance():
+            pass
+        def deposit():
+            pass
+        def withdraw():
+            pass
+        def exitAccount():
+            pass
+    
+    
+print(Program())
---
+++ pr.py
@@ -0,0 +1,7 @@
+import random
+acc = random.randint( 10 ** 15, 10 ** 16 -1)
+first_digit = acc // (10 ** (len(str(acc)) - 1))
+if acc[len(0):
+    print("Your account number is:",acc)
+else:
+    acc = random.randint( 10 ** 15, 10 ** 16 -1)
